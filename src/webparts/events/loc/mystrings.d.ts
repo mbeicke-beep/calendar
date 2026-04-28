3600812bda40f8eeaaba685185b086b60aebc937
Matthew Beicke <mbeicke@gi.gcsnet.biz>
2026-04-28 15:24:33 -0400

    intial

---
+++ src/webparts/events/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEventsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EventsWebPartStrings' {
+  const strings: IEventsWebPartStrings;
+  export = strings;
+}
